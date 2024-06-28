c55cdb4958ef764b2b9bed9e167963c14c18b07b
sitonlotus <liuzuolian@o-neg.com>
2024-06-28 10:47:35 +0800

    feat:添加面板分割组件、tag标签和文字溢出tooltip

---
+++ src/component/ZydTag.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="zyd-tag" :style="`--color:${color};${customSty}`">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ZydTag',
+  props: {
+    color: {
+      type: String,
+      default: '#2355D8',
+    },
+    customSty: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    rgbBackground() {
+      const colorValue = this.color.slice(1); // 移除'#'
+      const rgb = {
+        r: parseInt(colorValue.slice(0, 2), 16),
+        g: parseInt(colorValue.slice(2, 4), 16),
+        b: parseInt(colorValue.slice(4, 6), 16),
+      };
+      return `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 0.06)`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.zyd-tag {
+  $color: var(--color);
+  $opacity: 0.06;
+
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  padding: 4px 8px;
+  color: $color;
+  font-size: 14px;
+  font-family: PingFangSC, 'PingFang SC';
+  line-height: 18px;
+  background: v-bind('rgbBackground');
+  border: 1px solid $color;
+  border-radius: 4px;
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import ZydPage from './component/ZydPage.vue';
 import ZydOperateBtn from './component/ZydOperateBtn.vue';
 import ZydLargeScreenScale from './component/ZydLargeScreenScale.vue';
 import ZydTree from './component/ZydTree.vue';
+import ZydTag from './component/ZydTag.vue'
+import ZydSplitPane from './component/ZydSplitPane.vue'
+import ZydLongTextTooltip from './component/ZydLongTextTooltip.vue'
 
 export {
   ZydPage,
   ZydOperateBtn,
   ZydLargeScreenScale,
-  ZydTree
+  ZydTree,
+  ZydTag,
+  ZydSplitPane,
+  ZydLongTextTooltip
 }
