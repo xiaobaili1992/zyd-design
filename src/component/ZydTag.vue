977a4ff844d581f1a2d7f4054f6e509ce288d5fa
sitonlotus <liuzuolian@o-neg.com>
2024-07-04 17:06:40 +0800

    fix:修复颜色支持缩写形式，比如#ff0000可以缩写成#f00

--- src/component/ZydTag.vue
+++ src/component/ZydTag.vue
@@ -21,11 +21,18 @@ export default {
     rgbBackground() {
       if (this.color.includes('#')) {
         const colorValue = this.color.slice(1); // 移除'#'
-        const rgb = {
+        let rgb = {
           r: parseInt(colorValue.slice(0, 2), 16),
           g: parseInt(colorValue.slice(2, 4), 16),
           b: parseInt(colorValue.slice(4, 6), 16),
         };
+        if(colorValue.length == 3){
+          rgb = {
+            r: parseInt(colorValue.slice(0, 1).repeat(2), 16),
+            g: parseInt(colorValue.slice(1, 2).repeat(2), 16),
+            b: parseInt(colorValue.slice(2, 3).repeat(2), 16),
+          }
+        }
         return `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, 0.06)`;
       } else {
         const matches = this.color.match(/(\d+)/g);
