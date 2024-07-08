749b21b350ec4f4ccd5c938be12d772ed04f9522
lyc <2905669224@qq.com>
2024-07-08 11:25:10 +0800

    fix: 属性绑定改用v-bind

--- src/component/ZydOperateBtn.vue
+++ src/component/ZydOperateBtn.vue
@@ -2,10 +2,7 @@
   <div class="zyd-operate-btn">
     <span v-for="(item, index) in topThreeConfig" :key="item.key">
       <el-popover
-        :disabled="!item.tooltip"
-        :placement="item.tooltip?.placement||'top-start'"
-        :width="item.tooltip?.width||200"
-        :trigger="item.tooltip?.trigger||'hover'">
+        v-bind="item.tooltip || {}">
         <slot :name="item.key"></slot>
         <el-button type="text"  v-bind="item.attrs || {}" @click="(e) => commonFn(item, 'click', e)" slot="reference">{{ item.label }}</el-button>
       </el-popover>
