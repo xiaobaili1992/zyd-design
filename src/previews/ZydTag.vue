68f068c321dd804d8546b90840365153cbab31c4
sitonlotus <liuzuolian@o-neg.com>
2024-08-06 19:35:12 +0800

    fix:新增颜色优化

--- src/previews/ZydTag.vue
+++ src/previews/ZydTag.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <ZydTag color="#1cb866">我是tag</ZydTag>
+    <ZydTag color="red">红色</ZydTag>
+    <ZydTag color="#595959" customSty="  background: #f5f5f5;border: 1px solid rgba(140, 140, 140, 0.7);">
+      自定义离线
+    </ZydTag>
   </div>
 </template>
 
